07c05069fbc94cccbd6911cf5891b6cdf3603c7c
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-06-24 00:46:47 +0530

    Time: 665 ms (13.61%), Space: 70.7 MB (13.44%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     
+    /* Idea:
+    
+    sort the courses according to their deadline, 
+    so that we can finish the course which is gonna finish first
+    
+    add the course time in curr_time and push the course time in queue(priority_queue)
+    
+    if curr_time exceeds the deadline,it means we have to remove one course
+    remove the longest course ,i.e the course on top
+    also substract the value from curr_time
+    at last the size of queue  is the ans
+    
+    Time Complexity : O(N*log(N)) , Space Complexity : O(N)
+    */
+    
     static bool comp(vector<int>&a,vector<int>&b){
         return a[1]<b[1];
     }
@@ -11,6 +26,10 @@ public:
             return 0;
         
         sort(courses.begin(),courses.end(),comp);
+        
+        for(auto val:courses){
+            cout<<val[0]<<" "<<val[1]<<"\n";
+        }
         
         int curr_time=0;
         priority_queue<int>pq;
