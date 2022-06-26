a73f2f681cec55c92596a29b90fed6a3a194c70f
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-06-26 15:12:04 +0530

    Time: 123 ms (14.93%), Space: 42.3 MB (96.60%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+    /*Idea :
+    we have to pull out k card from start or end 
+    (so it will always make a sliding window in between)
+    
+    so instead,what we are goin to do is
+    we are finding a window size of n-k having minimum sun
+    so that when we susbtact it from total sum we get k cards with max sum 
+    */
+    
+    
     int maxScore(vector<int>& cardPoints, int k) {
      
         int n=cardPoints.size();
@@ -23,7 +33,6 @@ public:
             max_sum=max(max_sum,sum-window_sum);
         }
        
-        
         return max_sum;
     }
 };
