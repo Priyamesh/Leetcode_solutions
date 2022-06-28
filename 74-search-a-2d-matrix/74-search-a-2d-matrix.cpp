caae7d06bb5e7df282695611703ebe6c04534135
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-06-28 23:31:56 +0530

    Time: 14 ms (6.07%), Space: 9.6 MB (51.59%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,33 +1,55 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-     
-        
         int row=matrix.size();
         int col=matrix[0].size();
         
-        for(int i=0;i<row;i++)
+        int low=0;
+        int high=(row*col)-1;
+        
+        while(low<=high)
         {
-            if(target==matrix[i][0] || target==matrix[i][col-1])
+            int mid=low+(high-low)/2;
+            // cout<<mid<<" ";
+            
+            // a[i]=a[i/col][i/col]
+            int mid_val=matrix[mid/col][mid%col];
+            
+            if(target==mid_val)
                 return true;
-            if(target>matrix[i][0] && target < matrix[i][col-1])
-            {
-                int low=0;
-                int high=col-1;
-                while(low<=high)
-                {
-                    int mid=low+(high-low)/2;
-                    
-                    if(target==matrix[i][mid])
-                        return true;
-                    else if(target>matrix[i][mid])
-                        low=mid+1;
-                    else
-                        high=mid-1;
-                }
-            }
+            else if(target>mid_val)
+                low=mid+1;
+            else
+                high=mid-1;
         }
         return false;
-        
     }
 };
+
+
+// if we treat it as a matrix
+//         int row=matrix.size();
+//         int col=matrix[0].size();
+        
+//         for(int i=0;i<row;i++)
+//         {
+//             if(target==matrix[i][0] || target==matrix[i][col-1])
+//                 return true;
+//             if(target>matrix[i][0] && target < matrix[i][col-1])
+//             {
+//                 int low=0;
+//                 int high=col-1;
+//                 while(low<=high)
+//                 {
+//                     int mid=low+(high-low)/2;
+                    
+//                     if(target==matrix[i][mid])
+//                         return true;
+//                     else if(target>matrix[i][mid])
+//                         low=mid+1;
+//                     else
+//                         high=mid-1;
+//                 }
+//             }
+//         }
+//         return false;
