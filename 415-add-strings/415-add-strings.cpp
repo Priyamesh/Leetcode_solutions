1741fee0315f4758b17c08dcc051139a07114fa8
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-07-01 02:09:58 +0530

    Time: 10 ms (48.09%), Space: 6.7 MB (69.93%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -6,18 +6,15 @@ public:
         reverse(num2.begin(),num2.end());
         
         string res="";
-        int i=0;
-        int j=0;
-        int carry=0;
+        int i=0;int j=0;int carry=0;
+        int l1=num1.size();int l2=num2.size();
         
-        while(i<num1.size() || j<num2.size() || carry>0){
+        while(i<l1 || j<l2 || carry>0){
             
-            
-            
-            int n1=(i<num1.size())?num1[i]-'0':0;
+            int n1=(i<l1)?num1[i]-'0':0;
             i++;
             
-            int n2=(j<num2.size())?num2[j]-'0':0;
+            int n2=(j<l2)?num2[j]-'0':0;
             j++;
             
             int sum=n1+n2+carry;
