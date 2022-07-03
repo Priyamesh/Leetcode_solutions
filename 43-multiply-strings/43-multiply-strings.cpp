cfa196901c89a5f6944880232b02f99c0f1011a3
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-07-03 16:26:27 +0530

    Time: 18 ms (35.32%), Space: 6.7 MB (62.66%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -6,25 +6,30 @@ public:
         if (num1 == "0" || num2 == "0") return "0";
         
         // num1.size() + num2.size() == max no. of digits
-        vector<int> num(num1.size() + num2.size(), 0);
+        vector<int>out(num1.size() + num2.size(), 0);
         
-        // build the number by multiplying one digit at the time
-        for (int i = num1.size() - 1; i >= 0; --i) {
-            for (int j = num2.size() - 1; j >= 0; --j) {
-                num[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
-                num[i + j] += num[i + j + 1] / 10;
-                num[i + j + 1] %= 10;
+        reverse(num1.begin(),num1.end());
+        reverse(num2.begin(),num2.end());
+        
+        for(int i=0;i<num1.size();i++)
+        {
+            for(int j=0;j<num2.size();j++)
+            {
+                out[i+j]+=(num1[i]-'0')* (num2[j]-'0');
+                out[i+j+1]+=out[i+j]/10;
+                out[i+j]=out[i+j]%10;
             }
         }
+        int i=num1.size()+num2.size()-1;
+        while(i>=0 && out[i]==0){
+            i--;
+        }
         
-        // skip leading 0's
-        int i = 0;
-        while (i < num.size() && num[i] == 0) ++i;
-        
-        // transofrm the vector to a string
-        string res = "";
-        while (i < num.size()) res.push_back(num[i++] + '0');
-        
+        string res="";
+        for(;i>=0;i--)
+        {
+            res.push_back(out[i]+'0');
+        }
         return res;
     }
 };
