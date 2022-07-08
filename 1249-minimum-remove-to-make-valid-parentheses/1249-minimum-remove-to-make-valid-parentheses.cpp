74756ae67b8ed93fbced5346f3d3539e384af35d
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-07-08 23:11:25 +0530

    Time: 37 ms (46.57%), Space: 11.5 MB (56.42%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    
+    /*Idea:
+    while moving forward if we encounter '(' then we need to push its index in stack
+    if we encounter ')' then we need to check two things
+        1.if stack is empty, then its invalid so mark it as'*
+        2.if stack top has any opening,pop that since we found its closing
+    at last if there is something in stack it means, for those '(' there no ')'
+    so mark those index as '*'
+    
+    at last remove all * from the string and return
+    */
+    
     string minRemoveToMakeValid(string s) {
         
         stack<int>st;
