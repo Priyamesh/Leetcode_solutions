00cf67282e8989d83bebb2d662b4c3dd02df5884
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-07-11 22:40:24 +0530

    Time: 25 ms (64.50%), Space: 24.2 MB (59.62%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -11,23 +11,32 @@
  */
 class Solution {
 public:
+    /* Idea:
+        since inorder of BST gives us sorted array 
+        we can find Inorder and then return Kth Element
+        
+        just to make it more efficient 
+        we can skip the inorder once we have taken k elements
+    */
     
     vector<int>inorder;
     
-    void inorderTraversal(TreeNode* root)
+    void inorderTraversal(TreeNode* root,int k)
     {
         if(root==0)
             return;
         
-        inorderTraversal(root->left);
+        inorderTraversal(root->left,k);
         inorder.push_back(root->val);
-        inorderTraversal(root->right);
+        if(inorder.size()>k)
+            return;
+        inorderTraversal(root->right,k);
         
     }
     
     int kthSmallest(TreeNode* root, int k) {
         
-        inorderTraversal(root);
+        inorderTraversal(root,k);
         return inorder[k-1];
     }
 };
