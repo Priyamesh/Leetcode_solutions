eda274687704190b608434c0e2162fdf0b262554
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-08-22 14:09:32 +0530

    Time: 3 ms (59.97%), Space: 5.9 MB (26.88%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -9,5 +9,22 @@ public:
             return false;
         
         return isPowerOfTwo(n/2);
+        
+        
+        
+        //follow up
+        //POWER OF TWO
+	    //when a numerr is power of two it has only one 1 in it
+	    // and n-1 is all the 1 before it
+	    //so & of both will become 0
+        
+//         if(n<=0)
+//             return false;
+        
+//         if(n & n-1)
+//             return false;
+//         else
+//             return true;
+        
     }
 };
