7d800fca245acec8c9bab8a87946eeb162256a65
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-08-31 00:59:02 +0530

    Time: 2 ms (67.40%), Space: 7 MB (92.76%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
-    
-    /* Idea :
-    reverse the matrix and then take transpose of it
-    or 
-    take transpose and then reverse each row
-    */
-    
     void rotate(vector<vector<int>>& matrix) {
         
-        int row=matrix.size();
-        int col=matrix[0].size();
         
+        int n=matrix.size();
         
-        reverse(matrix.begin(),matrix.end());
-        
-        for(int i=0;i<row;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=i;j<col;j++)
+            for(int j=0;j<i;j++)
             {
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
+        for(int i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
         
     }
-    
-    
-//     void rotate_TR(vector<vector<int>>& matrix) {
-//         int n=matrix.size()-1;
-//         int m=matrix[0].size()-1;
-        
-//         for(int i=0;i<=n;i++)
-//         {
-//             for(int j=0;j<i;j++)
-//             {
-//                 swap(matrix[i][j],matrix[j][i]);
-//             }
-//         }
-//         for(int i=0;i<=n;i++)
-//         {
-//             reverse(matrix[i].begin(),matrix[i].end());
-//         }
-//     }
 };
