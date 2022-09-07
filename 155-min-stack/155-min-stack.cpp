9337c342fcd71b49af28a1a6ce3814fa75a4a1b2
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-09-07 23:34:16 +0530

    Time: 74 ms (5.14%), Space: 16.2 MB (99.50%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,31 +1,33 @@
 class MinStack {
 public:
     
-    stack<int>st;
-    multiset<int>mst;
-    
+    stack<int>st,minstack;
     MinStack() {
-        // st.clear();
-        // mst.clear();
+        //hello
     }
     
     void push(int val) {
         st.push(val);
-        mst.insert(val);
+        
+        if(minstack.empty() || val <=minstack.top())
+            minstack.push(val);
     }
     
     void pop() {
-        int key=st.top();
+        if(st.top() == minstack.top())
+            minstack.pop();
+        
         st.pop();
-        mst.erase(find(mst.begin(),mst.end(),key));
     }
     
     int top() {
+        
         return st.top();
+        
     }
     
     int getMin() {
-        return *mst.begin();
+        return minstack.top();
     }
 };
 
