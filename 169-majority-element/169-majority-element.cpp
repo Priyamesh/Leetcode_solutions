286180c3f8eec55cdb88dbdeb40299cb01f58edb
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-09-10 19:13:10 +0530

    Time: 30 ms (57.30%), Space: 19.6 MB (60.02%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -7,15 +7,13 @@ public:
         
         for(int i=0;i<nums.size();i++)
         {
-            if(count==0){
+            if(count==0)
                 majority=nums[i];
-            }
             
             if(nums[i]==majority)
                 count++;
             else
                 count--;
-            
         }
         
         return majority;
