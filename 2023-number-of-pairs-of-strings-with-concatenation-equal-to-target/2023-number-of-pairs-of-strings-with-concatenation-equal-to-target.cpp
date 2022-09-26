28a968724760af6a205b0359a434420b6dca354d
Priyamesh <47794863+Priyamesh@users.noreply.github.com>
2022-09-26 15:33:01 +0530

    Time: 28 ms (68.36%), Space: 12.3 MB (64.05%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //substr(pos,len)
+    //if we want the substring till last we just give postion
     int numOfPairs(vector<string>& nums, string target) {
         int n=target.size();
         int count=0;
@@ -14,6 +16,8 @@ public:
         {
             string prefix=target.substr(0,i+1);
             string suffix=target.substr(i+1,n-i);
+            // string suffix=target.substr(i+1,n-i);
+            
             // cout<<"prefix - "<<prefix<<"  suffix - "<<suffix<<"\n";
             
             if(prefix==suffix)
